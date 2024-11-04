5c49833365686f89d12ff81a66df7b754d92b491
Heshyo <Heshyo@users.noreply.github.com>
2024-11-04 19:35:12 +0800

    init

---
+++ Components/LoadingDialog.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MyApplication.Components;
+
+partial class LoadingDialog : ComponentBase
+{
+	[Parameter]
+	public DefaultFocus DefaultFocus { get; set; } = DefaultFocus.Element;
+
+	[CascadingParameter]
+	public MudDialogInstance SelfInstance { get; set; } = default!;
+
+	[Parameter]
+	public bool ShowCancel { get; set; } = true;
+
+	[Parameter]
+	public string Text { get; set; } = default!;
+
+	void Cancel()
+	{
+		SelfInstance.Close(DialogResult.Ok(false));
+	}
+}
